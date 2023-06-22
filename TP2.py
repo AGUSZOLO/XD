a208179286d973f92c0c19cb607a4b17dd20427f
AGUSZOLO <agustinzolorza59@gmail.com>
2023-06-22 17:19:33 -0300

    ULA

---
+++ TP2.py
@@ -0,0 +1,5 @@
+# Escribir un algoritmo que almacene en un archivo datos de alumnos
+# de BDI. almacena : 1.nombre, apellido   2.DNI    3. nota de los 6 tp 5,9,10,2,3,4  
+# 4 nota final de aprobacion/desaprobacion
+#Menu permita ingresar(estudiante), nota de los practicos
+#muestra estudiante aprobado/desaprobadoc
